033bd72003f723bdc77ea8c3c7db56539cfcccec
omaralian98 <52558137+omaralian98@users.noreply.github.com>
2023-12-07 16:35:59 +0300

    Add files via upload

---
+++ FirstAssignment.py
@@ -0,0 +1,32 @@
+import Game_Logic.SliderPuzzleGame as Game
+import Algorithms.AStar as AStar
+import time
+
+
+def print_goal_state():
+    print("Goal State: ")
+    goal = Game.SliderPuzzleGame(3)
+    goal.initialize_board([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+    goal.print_board()
+    print("\n")
+
+
+initial = Game.SliderPuzzleGame(3)
+board = [[1, 2, 3], [5, 6, 0], [7, 8, 4]]
+initial.initialize_board(board)
+print("Initial State: ")
+initial.print_board()
+print("\n")
+print_goal_state()
+start_time = time.time()
+path, discovered, visited = AStar.find_path(initial, Game.misplaced_tiles)
+mistime = (time.time() - start_time)
+start_time1 = time.time()
+path1, discovered1, visited1 = AStar.find_path(initial, Game.manhattan_distance)
+mantime = (time.time() - start_time1)
+print("Took: %s s to find" % format(mistime, '.3f'))
+print("Moves to solve using misplaced tiles heuristic: ", len(path))
+print("Expanded nodes to solve using misplaced tiles heuristic: ", visited)
+print("Took: %s s to find" % format(mantime, '.3f'))
+print("Moves to solve using manhattan distance heuristic: ", len(path1))
+print("Expanded nodes to solve using manhattan distance heuristic: ", visited1)
